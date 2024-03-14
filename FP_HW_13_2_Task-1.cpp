1cc9dd9ca7f30d7f2b70f6627124f6cf40e2c471
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-03-14 06:59:53 +0300

    версия 0_1

--- FP_HW_13_2_Task-1.cpp
+++ FP_HW_13_2_Task-1.cpp
@@ -2,9 +2,10 @@
 // чисел, скопировать один массив в другой.
 // Использовать в программе арифметику указателей для продвижения по массиву, а также оператор разыменования.
 
+
 #include <iostream>
-#include <windows.h>
 #include <random>
+#include <windows.h>
 using namespace std;
 
 template<typename T>
@@ -13,35 +14,49 @@ void FillArray(T* arr, int size);
 template<typename T>
 void PrintArray(T* arr, int size);
 
+template <typename T>
+void CopyArray(T* oldArr, T* newArr, int size);
+
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int size = 0;
-    cout << "Указать размер массива для заполнения : ";
-    cin >> size;
-    int* Array = new int[size];
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int size = 0;
+	cout << "Указать размер массива для заполнения : ";
+	cin >> size;
+	int* Array = new int[size];
+	int* ArrayCopy = new int[size];
 
-    FillArray(Array, size);
-    cout << "Заполненный массив ------------------- >>" << endl;
-    PrintArray(Array, size);
+	FillArray(Array, size);
+	
+	cout << "Заполненный массив ------------------- >>" << endl;
+	PrintArray(Array, size);
+
+	cout << "Скопированный массив ------------------- >>" << endl;
+	CopyArray(Array, ArrayCopy, size);
+	PrintArray(ArrayCopy, size);
 }
 
 template<typename T>
 void FillArray(T* arr, int size) {
-    random_device random;
-    for (int* i = arr; i < arr + size; ++i)
-    {
-        *i = random() % (100 + 50) - 50;
-    }
+	random_device random;
+	for (T* i = arr; i < arr + size; ++i){
+		*i = random() % (100 + 50) - 50;
+	}
 }
 
 template<typename T>
 void PrintArray(T* arr, int size) {
-    for (int* i = arr; i < arr + size; ++i)
-    {
-        cout << "[ " << *i <<" ]";
-    }
+	for (T* i = arr; i < arr + size; ++i){
+		cout << "[ " << *i << " ]";
+	}
 
-    cout << endl;
+	cout << endl;
+}
+
+template <typename T>
+void CopyArray(T* oldArr, T* newArr, int size) {
+	for (T* i = oldArr, *j = newArr; i < oldArr + size; ++i, ++j) {
+		*j = *i;
+	}
 }
